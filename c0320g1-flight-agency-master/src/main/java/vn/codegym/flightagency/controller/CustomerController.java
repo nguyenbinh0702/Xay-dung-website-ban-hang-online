d6072d633010d611d678f8b6c8d8495b2bfbf5fe
tramanh2000 <81839102+tramanh2000@users.noreply.github.com>
2021-05-15 13:53:39 +0700

    Delete c0320g1-flight-agency-master directory

--- c0320g1-flight-agency-master/src/main/java/vn/codegym/flightagency/controller/CustomerController.java
+++
@@ -1,4 +0,0 @@
-package vn.codegym.flightagency.controller;
-
-public class CustomerController {
-}
--- c0320g1-flight-agency-master/src/main/java/vn/codegym/flightagency/repository/BrandRepository.java
+++
@@ -1,7 +0,0 @@
-package vn.codegym.flightagency.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.codegym.flightagency.model.Branch;
-
-public interface BrandRepository extends JpaRepository<Branch, Long> {
-}
