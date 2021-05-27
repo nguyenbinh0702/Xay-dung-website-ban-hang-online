fb861a86bf9365e673985cd748db401f958083fc
Lê Nguyên Bình <75026941+nguyenbinh0702@users.noreply.github.com>
2021-05-27 15:54:55 +0700

    Delete webdidong directory

--- webdidong/autoload/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	session_start();
-	require_once __DIR__. '/../libraries/Database.php';
-	require_once __DIR__. '/../libraries/Function.php';
-
-	$db = new Database;
-
-	define ("ROOT", $_SERVER['DOCUMENT_ROOT']."/webdienthoai/public/uploads");
- ?>
--- webdidong/dang-xuat.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	session_start();
-	unset($_SESSION['name_user']);
-	unset($_SESSION['name_id']);
-
-	header("location:index.php");
- ?>
